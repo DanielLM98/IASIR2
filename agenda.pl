10c98ebbde21f360cda9a1bea6b76835e1476135
paulinohuerta <paulinohuerta@gmail.com>
2017-11-28 22:16:45 +0100

    Fixing

--- agenda.pl
+++ agenda.pl
@@ -1,17 +1,17 @@
 #!/usr/bin/perl
 print "Content-Type: text/html\n\n";
 if($ENV{'QUERY_STRING'} eq "") {
-  print "<h1>Elija el prefijo del numero deseado</h1>"
-    <form name=\"seach\" method=\"get\">";
-open F, "/tmp/agenda";
+  print "<h1>Elija el prefijo del numero deseado</h1>";
+  print "<form name=\"search\" method=\"get\">";
+  print "<input type=\"text\" name=\"prefijo\">";
+  print "<input type=\"submit\" value\"Buscar\" />";
+}else {
+  open F, "/tmp/agenda";
   while(<F>) {
     chomp;
      @prefijo=split(":");
-     $ar{$prefijo[1]}=$prefijo=[0];
+     $ar{$prefijo[0]}=$prefijo[1];
   }
- print "<input type="text" id"prefijo">";
- print "<input type=\"submit\" value\"Buscar\" />";
- }else
-  @arGet=split("=".$ENV{'QUERY_STRING'});
-    print "Los numeros de la Agenda son; $arGet[1]<br>";
+  @arGet=split("=",$ENV{'QUERY_STRING'});
+  print "Los numeros de la Agenda son; $ar{$arGet[1]}<br>";
 }
